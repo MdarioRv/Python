2c3dafa00c276ce0f5ef212e986157323884bd8a
MdarioRv <162085978+MdarioRv@users.noreply.github.com>
2024-04-08 22:56:44 -0600

    Add files via upload

---
+++ Proyectos/Suma de dos numeros.py
@@ -0,0 +1,13 @@
+
+#Funcion
+def suma (a, b):
+    return  a + b #La devolvemos
+
+
+print('Suma dos numeros: ')
+
+a = int(input("Ëscribe el primer numero: "))
+b = int(input("Escribe el segundo numero: "))
+
+print('Su resulatado es: ', suma(a, b))
+
